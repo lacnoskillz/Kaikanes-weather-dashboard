7bc26e6998e5b292fc4bbad72dd998fe345e5261
lacnoskillz <kaikane3457@hotmail.com>
2022-11-16 17:18:57 -0600

    added sections/containers for title of my website. user input. current weather condition section and 5day forecast section

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,8 @@
-console.log("tesing");
+weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={1d3ae5500d8fcb7af62c6c542bf6a203}';
+fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    });
